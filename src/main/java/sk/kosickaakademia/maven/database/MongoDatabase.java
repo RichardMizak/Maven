b3f5972f8e2ed7433f4d1d067236ab080bcf3889
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-03-29 22:22:58 +0200

    Update MongoDatabase.java

--- src/main/java/sk/kosickaakademia/maven/database/MongoDatabase.java
+++ src/main/java/sk/kosickaakademia/maven/database/MongoDatabase.java
@@ -1,21 +1,19 @@
 package sk.kosickaakademia.maven.database;
 
 
-
-import com.mongodb.client.MongoCollection;
-import org.json.simple.JSONObject;
+import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import com.mongodb.MongoClient;
 import sk.kosickaakademia.maven.Users.User;
 
-public class MongoDatabase {
+public class DatabaseMONGO {
     private static final MongoClient mongoClient = new MongoClient();
     private static MongoDatabase database;
     private static Document docs;
 
 
     public void insertUser(User user){
-            database= (MongoDatabase) mongoClient.getDatabase("myFirstDB");
+            database=mongoClient.getDatabase("myFirstDB");
             docs.append("fname", user.getFname());
             docs.append("lname", user.getLname());
             docs.append("age", user.getAge());
